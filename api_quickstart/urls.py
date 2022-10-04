36644c63037bfaf0908c6ce99cdd4531feef85eb
Gabriel-J-softmaktech <softmaktech@gmail.com>
2022-10-04 10:15:10 +0100

    Walk through DRF

---
+++ api_quickstart/urls.py
@@ -0,0 +1,15 @@
+from rest_framework import routers
+from django.urls import path, include
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewset)
+
+# wire up our API using automatic URL routing.
+# additionally, we include login URLs for browsable API.
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path("api-auth/", include('rest_framework.urls', namespace='rest_framework'))
+]
---
+++ quickstart/README.md
@@ -0,0 +1,11 @@
+### Self introduction to Django restframework.
+
+### A walk through restframework API documentation.
+
+### Expetations:
+
+### - Understanding how to get started with DRF
+
+### - Knowing how to work with CRUD using DRF
+
+### - Creating a ToDo Django App using Django and ReactJs.
